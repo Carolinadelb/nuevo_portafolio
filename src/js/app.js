a61cc8601472e01ad16a38c210ac2b7d0fccae9e
Carolinadelb <carolinadelgadob@gmail.com>
2018-09-30 18:50:46 -0300

    todos los cambios

--- src/js/app.js
+++ src/js/app.js
@@ -28,3 +28,26 @@ $(document).ready(function () {
     stickyToggle(sticky, stickyWrapper, $(window));
 });
 });
+
+//Flechita para arriba
+// ===== Scroll to Top ==== 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+//Video
